efb34d3d1f770e9c0bcfe990f5dc41443e9504ba
Ubuntu <ubuntu@ip-172-31-20-96.us-west-2.compute.internal>
2019-01-03 15:18:25 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
 # base image
 FROM ubuntu
-RUN sudo apt-get update
-RUN sudo apt-get install -y build-essential gcc g++
-ARG SRC=src.cpp
-ADD $SRC ~/
-ADD standard.cpp ~/
+RUN apt-get update
+RUN apt-get install -y build-essential gcc g++
+RUN apt-get install -y python
+#COPY std.cpp /home/
+RUN cd /home/
+#ADD https://s3-us-west-2.amazonaws.com/blanknoweb.com/src.cpp /home/
+#ADD https://s3-us-west-2.amazonaws.com/blanknoweb.com/std.cpp /home/
 #ADD demo_in.txt ~/
 #ADD demo_out.txt ~/
-RUN g++ $SRC -o src -std=c++11
-RUN g++ standard.cpp -o standard -std=c++11
-ADD test.py ~/
-RUN chmod a+x test.py
+#RUN g++ /home/src.cpp -o /home/src -std=c++11
+#RUN g++ /home/std.cpp -o /home/std -std=c++11
+#ADD test.py /home/
+ADD https://s3-us-west-2.amazonaws.com/blanknoweb.com/test.py /home/
+RUN chmod a+x /home/test.py
+#RUN /home/test.py
+ENTRYPOINT ["python","/home/test.py"]
--- test.py
+++ test.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 import commands
 import os
+import urllib
 import random
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+def download():
+    url_src = 'https://s3-us-west-2.amazonaws.com/blanknoweb.com/src.cpp'
+    url_std = 'https://s3-us-west-2.amazonaws.com/blanknoweb.com/std.cpp'
+    urllib.urlretrieve(url_src, '/home/src.cpp')
+    urllib.urlretrieve(url_std, '/home/std.cpp')
+
+def compiles():
+    std1, std2 = commands.getstatusoutput('g++ /home/std.cpp -o /home/std -std=c++11')
+    src1, src2 = commands.getstatusoutput('g++ /home/src.cpp -o /home/src -std=c++11')
+    if int(src1) != 0:
+        return False
+    else:
+        return True
+
 def gen_data():
-    count = random.random.randint(1, 49)
+    count = random.randint(1, 49)
     f = open('tmp.txt', mode='w')
     data = []
     for i in range(count):
         data.append(random.randint(1,2))
     data.append(0)
-    f.write(' '.join(data))
+    f.write(' '.join(map(str,data)))
     f.close()
     return data
 
@@ -17,12 +34,16 @@ def compare():
     m = 50
     for i in range(m):
         gen_data()
-        std1, std2 = getstatusoutput('./standard < tmp.txt')
-        res1, res2 = getstatusoutput('./src < tmp.txt')
-        if res1 != 0:
+        std1, std2 = commands.getstatusoutput('/home/std < tmp.txt')
+        res1, res2 = commands.getstatusoutput('/home/src < tmp.txt')
+        if int(res1) != 0:
             return False
         if int(std2) != int(res2):
             return False
     return True
 
- print(companre())
+download()
+if compiles():
+    print(compare())
+else:
+    print('compile error')
